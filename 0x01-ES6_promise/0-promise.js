8f614a805923e57e75a2988b28523ccc5b60d526
Samuel Obunge <samuelobunge@yahoo.com>
2024-06-27 12:12:33 +0300

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,10 @@
 export default function getResponseFromAPI() {
+  const success = true;
   return new Promise((resolve, reject) => {
-    resolve('Success');
+    if (success) {
+      resolve();
+    } else {
+      reject();
+    }
   });
 }
